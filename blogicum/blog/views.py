fbd44bf0ae4b6a059ccbd328d9aee3ee24f30f33
Denis <denis.Guselnikov2007@mail.ru>
2026-02-25 13:54:33 +0700

    fouth commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -4,13 +4,14 @@ from django.utils import timezone
 
 
 def index(request):
-    post_list = {'post_list': Post.objects.select_related(
-        'category'
-        ).filter(
+    post_list = {
+        'post_list': Post.objects.select_related('category').filter(
             is_published=True
-            ).filter(
-                created_at__lte=timezone.now()
-                ).filter(category__is_published=True)[0:5]}
+        ).filter(
+            created_at__lte=timezone.now()
+        ).filter(
+            category__is_published=True
+        )[:5]}
     return render(request, 'blog/index.html', post_list)
 
 
@@ -23,11 +24,12 @@ def post_detail(request, id):
 
 def category_posts(request, category_slug):
     template_name = 'blog/category.html'
-    category = get_object_or_404(Category, slug=category_slug, is_published=True)
-    post_list = Post.objects.select_related('category', 'location', 
-                                            'author').filter(category=category, 
-                                                             is_published=True, 
-                                                             pub_date__lte=timezone.now())
+    category = get_object_or_404(Category, slug=category_slug, 
+                                 is_published=True)
+    post_list = Post.objects.select_related(
+        'category', 'location', 'author').filter(
+            category=category, is_published=True,
+            pub_date__lte=timezone.now())
     context = {
         'category': category,
         'post_list': post_list
