096eacbb0122e5a8555603b2a6c4fc44bd6a49a6
Denis <denis.Guselnikov2007@mail.ru>
2026-02-25 13:28:06 +0700

    second commit

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -6,21 +6,21 @@ User = get_user_model()
 
 class Location(models.Model):
     name = models.CharField(max_length=256, verbose_name='Название места')
-    is_published = models.BooleanField(default=True, 
+    is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано',
                                        help_text='Снимите галочку, чтобы \
                                            скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True, 
+    created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Создано')
-    
+
     class Meta:
         verbose_name = 'местоположение'
         verbose_name_plural = 'Местоположения'
-        
+
     def __str__(self):
         return self.name
-    
-    
+
+
 class Category(models.Model):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
@@ -28,20 +28,20 @@ class Category(models.Model):
                             help_text='Идентификатор страницы для URL; \
                                 разрешены символы латиницы, цифры, дефис и \
                                     подчёркивание.')
-    is_published = models.BooleanField(default=True, 
+    is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано',
                                        help_text='Снимите галочку, чтобы \
                                            скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True, 
+    created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Создано')
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'Категории'
-        
+
     def __str__(self):
         return self.title
-  
+
 
 class Post(models.Model):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
@@ -50,25 +50,24 @@ class Post(models.Model):
                                     help_text='Если установить дату и время в \
                                         будущем — можно делать отложенные \
                                             публикации.')
-    author = models.ForeignKey(User, 
-                               on_delete=models.CASCADE, 
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
                                verbose_name='Автор публикации')
-    location = models.ForeignKey(Location, on_delete=models.SET_NULL, 
-                                 null=True, blank=True, 
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL,
+                                 null=True, blank=True,
                                  verbose_name='Местоположение')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,
                                  null=True, verbose_name='Категория')
-    is_published = models.BooleanField(default=True, 
+    is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано',
                                        help_text='Снимите галочку, чтобы \
                                            скрыть публикацию.')
-    created_at = models.DateTimeField(auto_now_add=True, 
+    created_at = models.DateTimeField(auto_now_add=True,
                                       verbose_name='Создано')
-    
+
     class Meta:
         verbose_name = 'публикация'
         verbose_name_plural = 'Публикации'
-        
+  
     def __str__(self):
         return self.title
-
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-from blog.models import Post, Category, Location, User
+from blog.models import Post, Category
 from django.utils import timezone
 
 
 def index(request):
     post_list = {'post_list': Post.objects.select_related('category')
-                .filter(is_published=True)
-                .filter(created_at__lte=timezone.now())
-                .filter(category__is_published=True)[0:5]}
+                                            .filter(is_published=True)
+                                            .filter(created_at__lte=timezone.now())
+                                            .filter(category__is_published=True)[0:5]}
     return render(request, 'blog/index.html', post_list)
 
 
@@ -22,7 +21,10 @@ def post_detail(request, id):
 def category_posts(request, category_slug):
     template_name = 'blog/category.html'
     category = get_object_or_404(Category, slug=category_slug, is_published=True)
-    post_list = Post.objects.select_related('category', 'location', 'author').filter(category=category, is_published=True, pub_date__lte=timezone.now())
+    post_list = Post.objects.select_related('category', 'location', 
+                                            'author').filter(category=category, 
+                                                             is_published=True, 
+                                                             pub_date__lte=timezone.now())
     context = {
         'category': category,
         'post_list': post_list
