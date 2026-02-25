5e1bae4593c9baa2a788d742c42b04afb58314ab
Denis <denis.Guselnikov2007@mail.ru>
2026-02-25 19:37:09 +0700

    11

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -27,14 +27,15 @@ class Category(models.Model):
     slug = models.SlugField(unique=True, verbose_name='Идентификатор',
                             help_text=(
                                 'Идентификатор страницы для URL; разрешены '
-                                'символы латиницы, цифры, дефис и подчёркивание.'
+                                'символы латиницы, цифры, дефис и '
+                                'подчёркивание.'
                             ))
     is_published = models.BooleanField(default=True,
                                        verbose_name='Опубликовано',
-                                       help_text='Снимите галочку, чтобы скрыть '
-                                       'публикацию.')
+                                       help_text='Снимите галочку, '
+                                       'чтобы скрыть публикацию.')
     created_at = models.DateTimeField(auto_now_add=True,
-                                     verbose_name='Добавлено')
+                                      verbose_name='Добавлено')
 
     class Meta:
         verbose_name = 'категория'
@@ -50,7 +51,7 @@ class Post(models.Model):
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
         help_text='Если установить дату и время в будущем можно '
-            'делать отложенные публикации.'
+                  'делать отложенные публикации.'
     )
     author = models.ForeignKey(
         User,
